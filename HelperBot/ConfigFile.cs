a70ad8e2d457a0aa9b4f68c7aba477ccef7e7c94
Ryozuki <clud6565@gmail.com>
2017-06-17 23:46:03 +0200

    enable regex, disable stats, not related to this plugin

--- HelperBot/ConfigFile.cs
+++ HelperBot/ConfigFile.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TShockAPI;
 
 namespace HelperBot
 {
@@ -12,7 +13,17 @@ namespace HelperBot
     {
         // Config variables here:
         public string PluginName = "HelperBot";
-        public bool UseMySQL = false;
+        public string BotName = "HelperBot";
+        public string BaseCommandName = "hb";
+        public string BotColor = "69,201,210";
+
+        public class QA
+        {
+            public string Question { get; set; }
+            public string Answer { get; set; }
+        }
+
+        public List<QA> QuestionsAndAnswers = new List<QA>();
         // End of config variables
 
         public static ConfigFile Read(string path)
@@ -20,6 +31,24 @@ namespace HelperBot
             if (!File.Exists(path))
             {
                 ConfigFile config = new ConfigFile();
+
+                config.QuestionsAndAnswers.Add(new QA()
+                {
+                    Question = @"(H|h)ow (can i|to) register(\?|)",
+                    Answer = string.Format("Use {0}spawn!", TShock.Config.CommandSpecifier)
+                });
+
+                config.QuestionsAndAnswers.Add(new QA()
+                {
+                    Question = @"(H|h)ow (do i go|i go) to the spawn(\?|)",
+                    Answer = string.Format("Use {0}spawn!", TShock.Config.CommandSpecifier)
+                });
+
+                config.QuestionsAndAnswers.Add(new QA()
+                {
+                    Question = @"(W|w)ho (made|created|coded) the (H|h)elper(B|b)ot(\?|)",
+                    Answer = "Ryozuki made it, you can find his web here: teeland.ovh"
+                });
 
                 File.WriteAllText(path, JsonConvert.SerializeObject(config, Formatting.Indented));
                 return config;
--- HelperBot/Main.cs
+++ HelperBot/Main.cs
@@ -9,6 +9,7 @@ using System.Data;
 using System.IO;
 using Mono.Data.Sqlite;
 using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace HelperBot
 {
@@ -19,7 +20,7 @@ namespace HelperBot
         // What does Name => string ? -> https://goo.gl/o7WAvJ
         public override string Name => "HelperBot";
         public override string Author => "Ryozuki";
-        public override string Description => "Replace me!!!";
+        public override string Description => "A bot with multiple utilities, such as stats gathering and a Q&A system.";
         public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
         #endregion
 
@@ -39,7 +40,7 @@ namespace HelperBot
         {
             LoadConfig();
             ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
-            ServerApi.Hooks.GamePostInitialize.Register(this, OnPostInitialize);
+            ServerApi.Hooks.ServerChat.Register(this, onChat, -666);
         }
 
         protected override void Dispose(bool disposing)
@@ -47,7 +48,6 @@ namespace HelperBot
             if (disposing)
             {
                 ServerApi.Hooks.GameInitialize.Deregister(this, OnInitialize);
-                ServerApi.Hooks.GamePostInitialize.Deregister(this, OnPostInitialize);
             }
             base.Dispose(disposing);
         }
@@ -56,22 +56,104 @@ namespace HelperBot
         private void OnInitialize(EventArgs args)
         {
             DB.Connect();
-            Commands.ChatCommands.Add(new Command("HelperBot.help".ToLower(), CHelp, "chelp")
+
+            Commands.ChatCommands.Add(new Command("helperbot.cmds", CommandHandler, Config.BaseCommandName)
             {
-                HelpText = "Usage: /chelp"
+                HelpText = string.Format("Usage: {0}{1} <command>", TShock.Config.CommandSpecifier, Config.BaseCommandName)
             });
         }
 
-        private void OnPostInitialize(EventArgs args)
+        private void onChat(ServerChatEventArgs e)
         {
+            string msg = e.Text;
 
+            var ply = TShock.Players[e.Who];
+
+            if (ply == null)
+                return;
+
+            msg = msg.ToLower();
+
+            if (Config.QuestionsAndAnswers.Any(x => Regex.Match(msg, x.Question).Success == true))
+            {
+                var answer = Config.QuestionsAndAnswers.Find(x => Regex.Match(msg, x.Question).Success == true).Answer;
+
+                var colorarr = Config.BotColor.Split(',');
+                try
+                {
+                    byte r = Byte.Parse(colorarr[0]);
+                    byte g = Byte.Parse(colorarr[1]);
+                    byte b = Byte.Parse(colorarr[2]);
+                    TShockAPI.Utils.Instance.Broadcast(string.Format("{0}: {1}", Config.BotName, answer), r, g, b);
+                }
+                catch (FormatException exc)
+                {
+                    TShock.Log.ConsoleError("[Helperbot] Error parsing the BotColor to a byte, are you sure it is in this format \"255,255,255\"?");
+                    TShock.Log.ConsoleError(exc.StackTrace);
+                }
+            }
+            else if (msg.Contains("how much is"))
+            {
+                int index = msg.LastIndexOf('s');
+                string op = msg.Substring(index + 1);
+                op = op.Replace("?", "");
+
+                try
+                {
+                    var colorarr = Config.BotColor.Split(',');
+                    double result = Convert.ToDouble(new DataTable().Compute(op, null));
+                    byte r = Byte.Parse(colorarr[0]);
+                    byte g = Byte.Parse(colorarr[1]);
+                    byte b = Byte.Parse(colorarr[2]);
+                    TShockAPI.Utils.Instance.Broadcast(string.Format("{0}: {1}", Config.BotName,
+                        "The result is: " + result.ToString()), r, g, b);
+                }
+                catch (System.Data.EvaluateException)
+                {
+                    // invalid operation
+                }
+                catch (FormatException a)
+                {
+                    TShock.Log.ConsoleError("[Helperbot] Error parsing the BotColor to a byte, are you sure it is in this format \"255,255,255\"?");
+                    TShock.Log.ConsoleError(a.StackTrace);
+                }
+            }
         }
         #endregion
 
         #region Commands
-        private void CHelp(CommandArgs args)
+        private void CommandHandler(CommandArgs e)
         {
-            args.Player.SendInfoMessage("Author, please change me.");
+            var ply = e.Player;
+            var cmd = "";
+
+            if (e.Parameters.Count != 0)
+            {
+                cmd = e.Parameters[0];
+            }
+
+            var args = e.Parameters.Skip(1).ToArray();
+            var colorarr = Config.BotColor.Split(',');
+            byte r = Byte.Parse(colorarr[0]);
+            byte g = Byte.Parse(colorarr[1]);
+            byte b = Byte.Parse(colorarr[2]);
+
+            switch (cmd)
+            {
+                case "stats":
+                default:
+                    {
+                        ply.SendMessage(String.Format("[PM] {0}: Usage: {1}{2} <command>",
+                                Config.BotName,
+                                TShock.Config.CommandSpecifier,
+                                Config.BaseCommandName),
+                                r, g, b);
+                        ply.SendMessage(String.Format("[PM] {0}: Commands: stats",
+                                Config.BotName),
+                                r, g, b);
+                        break;
+                    }
+            }
         }
         #endregion
     }
